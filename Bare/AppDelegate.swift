a2ced85eeaf9441de2ed6f7dc7d97fa0b903db34
Nils Fischer <n.fischer@viwid.com>
2016-04-30 16:51:15 +0200

    added window with label

--- Bare/AppDelegate.swift
+++ Bare/AppDelegate.swift
@@ -11,8 +11,20 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-        print("Hello World!")
+
+        let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window.rootViewController = UIViewController()
+        window.backgroundColor = UIColor.whiteColor()
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        let label = UILabel(frame: CGRect(x: 50, y: 50, width: 100, height: 44))
+        label.text = "Hello World!"
+        window.addSubview(label)
+        
         return true
     }
     
