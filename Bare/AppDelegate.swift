f68e12b2c9331d6552c32ea3611b5ccd14d56485
Nils Fischer <n.fischer@viwid.com>
2016-04-30 16:56:02 +0200

    added storyboard

--- Bare/AppDelegate.swift
+++ Bare/AppDelegate.swift
@@ -13,18 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-
-        let window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window.rootViewController = UIViewController()
-        window.backgroundColor = UIColor.whiteColor()
-        window.makeKeyAndVisible()
-        self.window = window
-        
-        let label = UILabel(frame: CGRect(x: 50, y: 50, width: 100, height: 44))
-        label.text = "Hello World!"
-        window.addSubview(label)
-        
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {        
         return true
     }
     
