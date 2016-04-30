e35cec8e71ffb87d19b837cf48c12837329a6d82
Nils Fischer <n.fischer@viwid.com>
2016-04-30 16:38:13 +0200

    initial commit with bare minimum

---
+++ Bare/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Bare
+//
+//  Created by Nils Fischer on 30.04.16.
+//  Copyright © 2016 iOS Dev Kurs Universität Heidelberg. All rights reserved.
+//
+
+print("Hello World!")
