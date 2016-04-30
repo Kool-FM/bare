c54ac7665ef490dc2e6c713ef2064d9b07e15731
Nils Fischer <n.fischer@viwid.com>
2016-04-30 17:12:48 +0200

    embedded in navigation controller

--- Bare/ViewController.swift
+++ Bare/ViewController.swift
@@ -9,12 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var greetingLabel: UILabel!
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.greetingLabel.text = "Hellooo 👋"
+        self.title = "Hellooo 👋"
     }
     
 }
